714c3082a32be00778faeb276f7859e7d7b4a3e1
CreditzKidSRC <Johannestowfiqi@outlook.com>
2025-07-12 07:25:15 +0200

    Create MenaceHubCZK.lua

---
+++ MenaceHubCZK.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/M-E-N-A-C-E/Menace-Hub/refs/heads/main/Free%20Sus%20Missile", true))()
+
+--[[
+         This is the script ig, if u see this i guess your cool heh.
+         - Love Creditz
+]]
